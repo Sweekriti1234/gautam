21fca13ea9726c1cbda7a239d405713f647e04a7
Sweekriti1234 <143303448+Sweekriti1234@users.noreply.github.com>
2023-09-06 18:35:09 -0700

    a

---
+++ anon.py
@@ -0,0 +1,31 @@
+# anonymous function/ lambda function
+
+'''greet = lambda: print("World")
+greet()
+
+#def abc(a, b, v):
+#lambda args: expression
+
+greet_user = lambda name: print("hello", name)
+greet_user("SWEEKRITI")
+
+CUBE = lambda : [X**3 for X in range(10)]
+lst = CUBE()
+print(lst)
+print(CUBE())'''
+
+'''a = []
+count = 0
+while count<10:
+    b = float(input("enter a number :"))
+    a.append(b)
+    count+=1
+
+print(a)'''
+
+nums= []
+for i in range(1,11):
+    
+    num = int(input(f"enter {i}st number:"))
+    nums.append(num)
+print(nums)
