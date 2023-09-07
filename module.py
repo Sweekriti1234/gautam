a1aa63cc69fc13b4255c038b374791f597979e94
Sweekriti1234 <143303448+Sweekriti1234@users.noreply.github.com>
2023-09-06 19:43:54 -0700

    g

---
+++ module.py
@@ -0,0 +1,27 @@
+'''#import calculator
+import calculator as cal
+calculator.user_input()'''
+#from calculator import user_input
+# calculator.user_inpuy without using"as" syntax
+
+#cal.user_input()
+#user_input()
+'''import math, random
+print(math. comb(5, 2))'''
+'''a = []
+count=0
+import random
+while count < 50:
+    i= random.randint(1,222)
+    a.append(i)
+    count += 1
+print(a)
+
+for i in range(0, 50):
+    b= random.randint(23, 345)
+    a.append(b)
+print(a)'''
+
+a= float(input("enter the weight in kg:"))
+pound = a*2.20
+print(f"{a} kg is {pound} pounds.")
