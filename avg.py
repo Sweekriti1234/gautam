e90f6f35998328b6331882904118f60ab6a215e7
Sweekriti1234 <143303448+Sweekriti1234@users.noreply.github.com>
2023-09-09 08:12:23 -0700

    home

---
+++ avg.py
@@ -0,0 +1,38 @@
+a = float(input("enter marks:"))
+b = float(input("enter marks:"))
+c = float(input("enter marks:"))
+avg = ( a + b + c)/3
+print(f"The average is {avg}")
+
+d = int(input("enter length of side:"))
+e = int(input("enter length of side:"))
+f = int(input("enter length of side:"))
+sarea = 2*d*e + 2*e*f + 2*f*d
+print(f"The surface area of prism is {sarea}")
+
+dis = 395000
+time = 9000
+speed = dis/time
+print(f"the speed of the plane is {speed}")
+
+l = 12
+w = 7
+d = 2
+vol= l*w*d 
+flow = 17
+t = vol/flow
+print(f'The time taken is {t}')
+
+sec = 24*60*60
+print(f'the seconds in a day is {sec}')
+
+vi = 0
+vf = 10
+ti = 20
+acc = (vf - vi)/ti
+print(f"The acceleration is {acc}")
+
+tem = float(input("enter temperature in celsius:"))
+fah = (tem * 1.8) + 32
+print(f'The temperature is {fah} degree F')
+
